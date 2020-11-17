7e1ede42b51ca64dbfbc93eae8b3f93b21b8e9c1
mingde <694104133@qq.com>
2020-11-17 20:08:33 +0900

    jya

--- xyz.php
+++ xyz.php
@@ -1,2 +1,6 @@
 <div>
+Error spawning PHP: Command failed: C:/php-7.4.12-Win32-vc15-x64/php.exe --version
+PHP Warning:  'vcruntime140.dll' 14.0 is not compatible with this PHP build linked with 14.16 in Unknown on line 0
+
+
 </div>
